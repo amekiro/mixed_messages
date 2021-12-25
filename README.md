af612b59be50d2810ec53643a8261d908157b74f
Caitlyn Sun <caitlyn.y.sun@gmail.com>
2021-12-25 14:55:07 -0500

    Version 2: JavaScript file complete

--- README.md
+++ README.md
@@ -1,32 +1,65 @@
 # MIXED MESSAGES README
 
 ## Scope of project
-Mixed Messages is a project from the Full Stack Engineer track on Codecademy, started by Caitlyn Sun December 25, 2021. Requirements of the project are:
+Mixed Messages is a project from the Full Stack Engineer track on Codecademy, started by Caitlyn Sun on December 25, 2021. Requirements of the project are:
 
 - User runs program --> user receives new randomized output
 - Databank of messages should have at minimum 3 entries
 
 This project is focused on JavaScript usage.
 
+## How to test JavaScript code
+1. Node.js and a command line application such as GitBash is required to test the code.
+2. Once the directory containing this README and the mixed_messages_script files is selected, run "$node mixed_messages_script.js" in the command line.
+3. Upon running the comman, a title should appear with "Story: ". Underneath that, a randomly generated story should be written out.
+
+## Required information
+Required information to run the JavaScript code are stored in the following arrays:
+
+- noun: Nouns are listed in the array as an array with the following variations:
+    - singular
+    - plural
+- verb: Verbs are listed in the array as an array with the following variations:
+    - present tense
+    - past tense
+    - -ing
+- adj: Adjectives are listed in the array as an array with the following variations:
+    - dictionary form (default)
+    - comparison (est)
+- adv: Adverbs do not have a variation, and thus are listed as a single array
+
+For nouns, verbs and adjections, the matrix array allows for additions should a mad lib need a variation not currently listed.
+
+## How JavaScript code functions
+Upon running $node mixed_messages_script.js, the following steps are run:
+
+1. An array of stories are created
+
+    A. Depending on the number of nouns, verbs, adjectives and adverbs requested, storyFac will either generate lists of random words or alert the user that there are not enough words in the word bank to create the story.
+    B. Words are added to the anonymous function that is referenced in storyFac.
+2. Array of stories is filtered to remove unsuccessful stories.
+3. Random index is created based on the number of successful stories.
+4. A random story is printed to the console.
+
 ## Task List
-1. Plan project objectives and timeline
+1. Plan project objectives and timeline - COMPLETE
     
-    - Generate madlibs for a story
+    - Generate mad libs for a story
     - Should be completed by December 26, 2021
 
-2. Decide on message outputs
+2. Decide on message outputs - COMPLETE
 
-    - Choose madlibs template - randomized
+    - Choose mad libs template - randomized
     - Choose words - randomzed
 
-3. Set up Git for version control
-4. Write JavaScript code for functionality and test with Node.js
+3. Set up Git for version control - COMPLETE
+4. Write JavaScript code for functionality and test with Node.js - COMPLETE
 
     - Needed functions
 
-        - Select madlibs template
-        - Select madlibs words
+        - Select mad libs template
+        - Select mad libs words
         - Return completed story
 
-5. Set up message storage
-6. Assemble all components
+5. Set up message storage - COMPLETE
+6. Assemble all components - COMPLETE
--- mixed_messages_script.js
+++ mixed_messages_script.js
@@ -1,1 +1,185 @@
-console.log("Hello world");
+const noun = [
+    ["Tiger", "Tigers"], // 0: Singular, 1: Plural
+    ["Lion", "Lions"],
+    ["Pig", "Pigs"],
+    ["Mouse", "Mice"],
+    ["Coin", "Coins"],
+    ["Game", "Games"]
+]
+
+const verb = [
+    ["Jump", "Jumped", "Jumping"], // 0: Present tense, 1: Past tense, 2: -ing
+    ["Rip", "Ripped", "Ripping"],
+    ["See", "Saw", "Seeing"],
+    ["Laugh", "Laughed", "Laughing"]
+]
+
+const adj = [
+    ["Dark", "Darkest"], // 0: Default, 1: Comparison
+    ["Bright", "Brightest"],
+    ["Fun", "Funnest"],
+    ["Meek", "Meekest"]
+]
+
+const adv =[
+    "Quickly",
+    "Silently"
+]
+
+const vowel = ["a", "e", "i", "o", "u"];
+
+const findWord = (arr, count) => {
+    if (arr.length < count) {
+        return "ERR";
+    }
+
+    const words = [];
+
+    while (count > 0) {
+        let index = Math.floor(Math.random() * arr.length);
+        let val = arr[index];
+        if (Array.isArray(val)) {
+            for (let i = 0; i < val.length; i++) {
+                val[i] = val[i].toLowerCase();
+            }
+        } else {
+            val = val.toLowerCase();
+        }
+
+        words.push(val);
+        arr.splice(index, 1);
+
+        count--;
+    }
+    return words;
+}
+
+const storyFac = (title, nc, vc, ac, av, storyFun) => {
+    let nouns = findWord(noun.map(x => x), nc);
+    let verbs = findWord(verb.map(x => x), vc);
+    let adjs =  findWord(adj.map(x => x), ac);
+    let advs = findWord(adv.map(x => x), av);
+
+    let err = "";
+
+    if (nouns === "ERR") {
+        err += `
+        There are not enough words in the noun bank. Please enter a number less than ${noun.length++}; `;
+    }
+
+    if (verbs === "ERR") {
+        err += `
+        There are not enough words in the verb bank. Please enter a number less than ${verb.length++}; `;
+    }
+
+    if (adjs === "ERR") {
+        err += `
+        There are not enough words in the adjective bank. Please enter a number less than ${adj.length++};`;
+    }
+
+    if (advs === "ERR") {
+        err += `
+        There are not enough words in the adverb bank. Please enter a number less than ${adv.length++};`;
+    }
+
+    if (err != "") {
+        console.log(`Unable to create story for ${title}: ` + err);
+        
+        return;
+    }
+
+    return `Story:  ${title}
+    
+    ${storyFun(nouns, verbs, adjs, advs)}`;
+}
+
+const story = [
+    storyFac("A Day At The Zoo!", 3, 3, 4, 2, function(nouns, verbs, adjs, advs) {
+        let str = `Today I went to the zoo. I saw a${vowel.some(x => adjs[0][0][0] === x) ? "n" : ""} ${adjs[0][0]} ${nouns[0][0]}`;
+        adjs.shift();
+        nouns.shift();
+
+        str += ` jumping up and down in its tree. He ${verbs[0][1]} ${advs[0]}`;
+        verbs.shift();
+        advs.shift();
+
+        str += ` through the large tunnel that led to its ${adjs[0][0]} ${nouns[0][0]}.`;
+        adjs.shift();
+        nouns.shift();
+        
+        str += ` I got some peanuts and passed them through the cage to a gigantic gray ${nouns[0][0]}`;
+        nouns.shift();
+
+        str += ` towering above my head. Feeding that animal made me hungry. I went to get a ${adjs[0][0]}`;
+        adjs.shift();
+
+        str += ` scoop of ice cream. It filled my stomach. Afterwards I had to ${verbs[0][0]} ${advs[0]}`;
+        verbs.shift();
+        advs.shift();
+
+        str += ` to catch our bus. When I got home I ${verbs[0][1]}`;
+        verbs.shift();
+        
+        str += ` my mom for a ${adjs[0][0]} day at the zoo.`;
+
+        return str;
+    }),
+    storyFac("The Fun Park!", 2, 2, 3, 2, function(nouns, verbs, adjs, advs) {
+        let str = `Today, my fabulous camp group went to a${vowel.some(x => adjs[0][0][0] === x) ? "n" : ""} ${adjs[0][0]}`;
+        adjs.shift();
+
+        str += ` amusement park. It was a fun park with lots of cool ${nouns[0][1]}`;
+        nouns.shift();
+
+        str += ` and enjoyable play structures. When we got there, my kind counselor shouted loudly, "Everybody off the ${nouns[0][0]}."`;
+        nouns.shift();
+        
+        str += ` We all pushed out in a terrible hurry. My counselor handed out yellow tickets, and we scurried in. I was so excited! I couldn't figure out what exciting thing to do first. I saw a scary roller coaster I really liked so, I ${advs[0]}`;
+        advs.shift();
+        
+        str += ` ran over to get in the long line that had about ${Math.floor(Math.random() * 100)} people in it. When I finally got on the roller coaster I was ${verbs[0][1]}.`;
+        verbs.shift();
+        
+        str += ` In fact I was so nervous my two knees were knocking together. This was the ${adjs[0][1]}`;
+        adjs.shift();
+
+        str += ` ride I had ever been on! In about two minutes I heard the crank and grinding of the gears. That\'s when the ride began! When I got to the bottom, I was a little ${verbs[0][1]}`;
+        verbs.shift();
+        
+        str += ` but I was proud of myself. The rest of the day went ${advs[0]}.`;
+        advs.shift();
+        
+        str += ` It was a${vowel.some(x => adjs[0][0][0] === x) ? "n" : ""} ${adjs[0][0]} day at the fun park. `;
+
+        return str;
+    }),
+    storyFac("At The Arcade!", 6, 12, 1, 1, function(nouns, verbs, adjs, advs) {
+        let str = `When I go to the arcade with my ${nouns[0][1]}`;
+        nouns.shift();
+        
+        str += ` there are lots of games to play. I spend lots of time there with my friends. In the game X-Men you can be different ${nouns[0][1]}.`;
+        nouns.shift();
+        
+        str += ` The point of the game is to ${verbs[0][0]}`;
+        verbs.shift();
+        
+        str += ` every robot. You also need to save people. Then you can go to the next level. In the game Star Wars you are Luke Skywalker and you try to destroy every ${nouns[0][0]}.`;
+        nouns.shift();
+        
+        str += ` In a car racing/motorcycle racing game you need to beat every computerized vehicle that you are ${verbs[0][2]}`;
+        verbs.shift();
+        
+        str += ` against. There are a whole lot of other cool games. When you play some games you win ${nouns[0][1]}`;
+        nouns.shift();
+        
+        str += ` for certain scores. Once you're done you can cash in your tickets to get a big ${nouns[0][0]}.`;
+        nouns.shift();
+        
+        str += ` You can save your ${nouns[0][0]} for another time. When I went to this arcade I didn't believe how much fun it would be. So far I have had a lot of fun every time I've been to this great arcade! `;
+
+        return str;
+    })
+].filter(x => x);
+
+const randInd = Math.floor(Math.random() * story.length);
+console.log(story[randInd]);
